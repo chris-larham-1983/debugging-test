251cf6d407d0178849778bb77f90fbcdbca12a71
chris-larham-1983 <chrislarham@outlook.com>
2022-04-03 22:00:44 +0100

    Updated homepage in package.json ready for deploy

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -18,39 +18,40 @@ const Navigation = () => {
   }, []);
 
   return (
-    <nav>
-      <div className="nav-logo">
-        <img src={Logo} alt="Petlover" />
-        <Search />
-      </div>
-      <ul className="nav-links">
-        <li key={'all'}>
-          {/* This link should have an activeClassName and exact prop */}
-          <NavLink to="/"
-            className="nav-link"
-            activeClassName='nav-link-active'
-            exact
-          >
-            All Pets
-          </NavLink>
-        </li>
-        {petTypes
-          ? petTypes.map((type) => (
-              <li key={type.name}>
-                {/* These links should have an activeClassName prop */}
-                <NavLink to={`/${type._links.self.href.split('/').pop()}`}
-                  key={type.name}
-                  className="nav-link"
-                  activeClassName='nav-link-active'
-                >
-                  {type.name}s
-                </NavLink>{' '}
-              </li>
-            ))
-          : 'Loading...'}
-      </ul>
-    </nav>
+      <nav>
+        <div className="nav-logo">
+          <img src={Logo} alt="Petlover" />
+          <Search />
+        </div>
+        <ul className="nav-links">
+          <li key={'all'}>
+            {/* This link should have an activeClassName and exact prop */}
+            <NavLink to="/"
+                     className="nav-link"
+                     activeClassName='nav-link-active'
+                     exact
+            >
+              All Pets
+            </NavLink>
+          </li>
+          {petTypes
+              ? petTypes.map((type) => (
+                  <li key={type.name}>
+                    {/* These links should have an activeClassName prop */}
+                    <NavLink to={`/${type._links.self.href.split('/').pop()}`}
+                             key={type.name}
+                             className="nav-link"
+                             activeClassName='nav-link-active'
+                    >
+                      {type.name}s
+                    </NavLink>{' '}
+                  </li>
+              ))
+              : 'Loading...'}
+        </ul>
+      </nav>
   );
 };
 
 export default Navigation;
+
